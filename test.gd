e3e9dbf96aaae7c6c9dcdb23c0ee68d883fdb4bc
Spectralo <arthur.hardy@mageekbox.net>
2023-01-13 21:05:34 +0100

    text.gd

--- test.gd
+++ test.gd
@@ -5,10 +5,11 @@ extends Node
 # var a = 2
 # var b = "text"
 
-
+var mathis = "con"
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	if mathis == "con":
+		mathis = "intelligent" # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
