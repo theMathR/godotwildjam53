ec9394c8662dddd7ca31c2325963c2aa33febfa5
Spectralo <arthur.hardy@mageekbox.net>
2023-01-13 22:40:25 +0100

    Acueil

--- test.gd
+++ test.gd
@@ -4,12 +4,13 @@ extends Node
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+var a = "a"
 var mathis = "con"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if mathis == "con":
 		mathis = "intelligent" # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
