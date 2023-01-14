72c067c5dbe521fea08568867232ec35d61f9cc8
--get <mathis.feraut@gmail.com>
2023-01-14 18:24:36 +0100

    Créé le joueur

---
+++ Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+const R_SPEED = 0.05
+const SPEED = 200
+onready var screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
+	if Input.is_action_pressed("ui_left"):
+		rotate(-R_SPEED)
+	if Input.is_action_pressed("ui_right"):
+		rotate(R_SPEED)
+	if Input.is_action_pressed("ui_up"):
+		move_and_slide(Vector2.RIGHT.rotated(rotation)*SPEED)
+	elif Input.is_action_pressed("ui_down"):
+		move_and_slide(Vector2.LEFT.rotated(rotation)*SPEED)
+	position.x = clamp(position.x, 0, screen_size.x)
+	position.y = clamp(position.y, 0, screen_size.y)
