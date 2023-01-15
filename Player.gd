46353360955bada1969ec799b6ec1784b9740bad
Spectralo <arthur.hardy@mageekbox.net>
2023-01-15 14:17:08 +0100

    Manette

--- Player.gd
+++ Player.gd
@@ -6,13 +6,13 @@ onready var screen_size = get_viewport_rect().size
 var attracted = null
 
 func _physics_process(_delta):
-	if Input.is_action_pressed("ui_left"):
+	if Input.is_action_pressed("move_left"):
 		rotate(-R_SPEED)
-	if Input.is_action_pressed("ui_right"):
+	if Input.is_action_pressed("move_right"):
 		rotate(R_SPEED)
-	if Input.is_action_pressed("ui_up"):
+	if Input.is_action_pressed("move_up"):
 		move_and_slide(Vector2.RIGHT.rotated(rotation)*SPEED)
-	elif Input.is_action_pressed("ui_down"):
+	elif Input.is_action_pressed("move_down"):
 		move_and_slide(Vector2.LEFT.rotated(rotation)*SPEED)
 	position.x = clamp(position.x, 0, screen_size.x)
 	position.y = clamp(position.y, 0, screen_size.y)
