63e4eb1148aa98ccdcd8fec728de2e07595a9d2f
theMathR <56732467+theMathR@users.noreply.github.com>
2023-01-17 18:02:56 +0100

    Update World.gd

--- World.gd
+++ World.gd
@@ -2,6 +2,31 @@ extends Node2D
 
 export(PackedScene) var atom_scene
 
+const atom_sprites = [
+	preload("res://assets/1.png"),
+	preload("res://assets/2.png"),
+	preload("res://assets/3.png"),
+	preload("res://assets/4.png"),
+	preload("res://assets/5.png"),
+	preload("res://assets/6.png"),
+	preload("res://assets/7.png"),
+	preload("res://assets/8.png"),
+	preload("res://assets/9.png"),
+	preload("res://assets/10.png"),
+	preload("res://assets/11.png"),
+	preload("res://assets/12.png"),
+	preload("res://assets/13.png"),
+	preload("res://assets/14.png"),
+	preload("res://assets/15.png"),
+	preload("res://assets/16.png"),
+	preload("res://assets/17.png"),
+	preload("res://assets/18.png"),
+	preload("res://assets/19.png"),
+	preload("res://assets/20.png"),
+	preload("res://assets/21.png"),
+	preload("res://assets/22.png")
+]
+
 func _ready():
 	randomize()
 	$AtomTimer.start()
@@ -19,6 +44,10 @@ func _on_AtomTimer_timeout():
 
 	# Set the atom's position to a random location.
 	atom.position = atom_spawn_location.position
+	
+	# Set the atom's type and sprite
+	atom.atom_type = randi() % 22
+	atom.get_node('CollisionShape2D/Sprite').set_texture(atom_sprites[atom.atom_type])
 
 	# Choose the velocity for the atom.
 	var velocity = Vector2(rand_range(150.0, 250.0), 0.0)
@@ -30,3 +59,4 @@ func _on_AtomTimer_timeout():
 
 	# Spawn the atom by adding it to the Main scene.
 	add_child(atom)
+
